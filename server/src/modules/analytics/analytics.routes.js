93a00b28fefd758bc20f8e51f6ebd73ea6d9ec47
Niranjan Shanbhag <nshanbhag345@gmail.com>
2026-05-09 09:42:28 +0530

    Update analytics.routes.js

--- server/src/modules/analytics/analytics.routes.js
+++ server/src/modules/analytics/analytics.routes.js
@@ -4,8 +4,9 @@ const analyticsController = require('./analytics.controller');
 
 const router = express.Router();
 
-router.get('/analytics/dashboard', authMiddleware, analyticsController.getDashboardSummary);
-router.get('/analytics/progress', authMiddleware, analyticsController.getProgress);
-router.post('/analytics/activity', authMiddleware, analyticsController.trackActivity);
+// Routes are mounted under /analytics in routes/index.js
+router.get('/dashboard', authMiddleware, analyticsController.getDashboardSummary);
+router.get('/progress', authMiddleware, analyticsController.getProgress);
+router.post('/activity', authMiddleware, analyticsController.trackActivity);
 
 module.exports = router;
