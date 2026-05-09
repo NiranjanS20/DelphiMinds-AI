73750aaff9d329c3ab4072c2fe419f6f132ae9c7
Niranjan Shanbhag <nshanbhag345@gmail.com>
2026-05-09 09:41:51 +0530

    Refactor career routes to remove duplicates
    Removed duplicate career routes and added a comment about route mounting.

--- server/src/modules/career/career.routes.js
+++ server/src/modules/career/career.routes.js
@@ -4,9 +4,8 @@ const careerController = require('./career.controller');
 
 const router = express.Router();
 
+// Routes are mounted under /career in routes/index.js
 router.get('/recommendations', authMiddleware, careerController.getRecommendations);
 router.get('/skill-gap', authMiddleware, careerController.getSkillGap);
-router.get('/career/recommendations', authMiddleware, careerController.getRecommendations);
-router.get('/career/skill-gap', authMiddleware, careerController.getSkillGap);
 
 module.exports = router;
