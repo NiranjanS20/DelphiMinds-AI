5ffcb7341788daad9a15d0b3193786302358380f
Niranjan Shanbhag <nshanbhag345@gmail.com>
2026-05-14 21:29:39 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -5,40 +5,40 @@ const morgan = require('morgan');
 
 const env = require('./config/env');
 const { getUploadRoot } = require('./utils/uploadPaths');
-const logger = require('./utils/logger');␊
-const apiRoutes = require('./routes');␊
-const { generalApiLimiter } = require('./middleware/rateLimiter');␊
-const { notFoundHandler, errorHandler } = require('./middleware/error.middleware');␊
-␊
-const app = express();␊
-␊
-app.use(␊
-  cors({␊
-    origin: (origin, callback) => {␊
-      if (!origin || env.allowedOrigins.includes(origin)) {␊
-        return callback(null, true);␊
-      }␊
-      return callback(new Error('Not allowed by CORS'));␊
-    },␊
-    credentials: true,␊
-  })␊
-);␊
-␊
-app.use(helmet());␊
-app.use(morgan('combined', { stream: logger.requestLoggerStream }));␊
-app.use(express.json({ limit: '2mb' }));␊
-app.use(express.urlencoded({ extended: true }));␊
-␊
-app.use(␊
-  '/uploads',␊
+const logger = require('./utils/logger');
+const apiRoutes = require('./routes');
+const { generalApiLimiter } = require('./middleware/rateLimiter');
+const { notFoundHandler, errorHandler } = require('./middleware/error.middleware');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || env.allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true,
+  })
+);
+
+app.use(helmet());
+app.use(morgan('combined', { stream: logger.requestLoggerStream }));
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  '/uploads',
   express.static(getUploadRoot(), {
-    maxAge: '1d',␊
-  })␊
-);␊
-␊
-app.use('/api', generalApiLimiter, apiRoutes);␊
-␊
-app.use(notFoundHandler);␊
-app.use(errorHandler);␊
-␊
-module.exports = app;␊
+    maxAge: '1d',
+  })
+);
+
+app.use('/api', generalApiLimiter, apiRoutes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+module.exports = app;
